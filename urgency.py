6ea35385779afe0c8939fe40ae0b3b658f5c3572
clin <clin@splunk.com>
2014-01-21 17:26:41 +0800

    init the branch

---
+++ urgency.py
@@ -0,0 +1,28 @@
+from TestlinkWeb import TestlinkCase
+from TestlinkWeb import TestlinkWeb
+import getpass
+import optparser
+
+
+def main():
+    """
+    Main process
+    """
+    LOGIN_NAME = raw_input("Please enter your testlink login name: ")
+    LOGIN_PWD = getpass.getpass("Please enter your testlink login password: ")
+
+    testlink_web = TestlinkWeb()
+    # if testlink_web.login(LOGIN_NAME, LOGIN_PWD):
+
+
+if __name__ == '__main__':
+    # LOGIN_NAME = raw_input("Please enter your testlink login name: ")
+    # LOGIN_PWD = getpass.getpass("Please enter your testlink login password: ")
+    LOGIN_NAME = "clin"
+    LOGIN_PWD = "2jiidgli"
+
+    testlink_web = TestlinkWeb()
+    testlink_web.login(LOGIN_NAME, LOGIN_PWD)
+
+    case = TestlinkCase("1002")
+    testlink_web.getCasePath(case)
